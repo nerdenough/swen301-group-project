25ec4d1db47e619ec6e39e7d190be37d3e63f156
Brendan Goodenough <brendan@goodenough.nz>
2016-06-08 17:01:03 +1200

    Add registration route to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,38 @@ router.post('/login', function(req, res) {
 
 // POST: /auth/register
 router.post('/register', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var response = {};
 
+  if (!email || !password) {
+    return res.sendStatus(500);
+  }
+
+  var sql = 'SELECT id FROM users WHERE email=?';
+  req.db.query(sql, email, function(err, rows) {
+    if (err) {
+      return res.sendStatus(500);
+    } else if (rows.length) {
+      response.error = 'User already exists';
+      return res.send(response);
+    }
+
+    var data = {
+      email: email,
+      password: password
+    };
+
+    sql = 'INSERT INTO users SET ?';
+    req.db.query(sql, data, function(err, result) {
+      if (err) {
+        return res.sendStatus(500);
+      }
+
+      response.success = true;
+      res.send(response);
+    });
+  });
 });
 
 // POST: /auth/logout
