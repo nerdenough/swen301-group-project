55009b3b0692cebbe0e9c2ef56961c8b13125f6b
Gordon Adams <gordonadamsnz@gmail.com>
2016-06-19 16:05:37 +1200

    First graphs
    Added 3 Pi charts
    Origin
    Destination
    Priority

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -32,7 +32,52 @@ $('document').ready(function(){
 
 
       //Destinations
+      destin_dim = ndx.dimension(function(d){
+        return d.to;
+      });
+      destin_group = destin_dim.group().reduceSum(function(d) {
+        return 1;
+      });
 
+      destin_PI_chart = dc.pieChart('#destinationPiChart');
+      destin_PI_chart
+        .dimension(destin_dim)
+        .group(destin_group)
+        .legend(dc.legend())
+        .height(300);
+
+      //Origin
+      destin_dim = ndx.dimension(function(d){
+        return d.from;
+      });
+      destin_group = destin_dim.group().reduceSum(function(d) {
+        return 1;
+      });
+
+      destin_PI_chart = dc.pieChart('#originPiChart');
+      destin_PI_chart
+        .dimension(destin_dim)
+        .group(destin_group)
+        .legend(dc.legend())
+        .height(300);
+
+      //Origin
+      destin_dim = ndx.dimension(function(d){
+        return d.priority;
+      });
+      destin_group = destin_dim.group().reduceSum(function(d) {
+        return 1;
+      });
+
+      destin_PI_chart = dc.pieChart('#priorityPiChart');
+      destin_PI_chart
+        .dimension(destin_dim)
+        .group(destin_group)
+        .legend(dc.legend())
+        .height(300);
+
+
+        dc.renderAll();
     },
     error: function (err){
       debugger;
