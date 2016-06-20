26a5ba2eca87601d3a08347c6605c4f76f86a407
Brendan Goodenough <brendan@goodenough.nz>
2016-06-20 16:56:32 +1200

    Update authentication routes to use tokens

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var crypto = require('crypto');
 var router = express.Router();
 
 // POST: /auth/login
@@ -11,7 +12,7 @@ router.post('/login', function(req, res) {
     return res.sendStatus(500);
   }
 
-  var sql = 'SELECT password FROM users WHERE email=?';
+  var sql = 'SELECT password FROM users WHERE email = ?';
   req.db.query(sql, email, function(err, rows) {
     if (err) {
       return res.sendStatus(500);
@@ -20,29 +21,32 @@ router.post('/login', function(req, res) {
       return res.send(response);
     }
 
-    var row = rows[0];
-
-    if (password === row.password) {
-      response.success = true;
+    if (rows[0].password !== password) {
+      response.error = 'Invalid username or password';
       return res.send(response);
     }
 
-    response.error = 'Invalid username or password';
-    res.send(response);
+    createToken(req.db, email, function(token) {
+      response.success = true;
+      response.token = token;
+      res.send(response);
+    });
   });
 });
 
 // POST: /auth/register
 router.post('/register', function(req, res) {
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
   var email = req.body.email;
   var password = req.body.password;
   var response = {};
 
-  if (!email || !password) {
+  if (!firstname || !lastname || !email || !password) {
     return res.sendStatus(500);
   }
 
-  var sql = 'SELECT id FROM users WHERE email=?';
+  var sql = 'SELECT id FROM users WHERE email = ?';
   req.db.query(sql, email, function(err, rows) {
     if (err) {
       return res.sendStatus(500);
@@ -52,8 +56,11 @@ router.post('/register', function(req, res) {
     }
 
     var data = {
+      firstname: firstname,
+      lastname: lastname,
       email: email,
-      password: password
+      password: password,
+      registered: new Date().getTime()
     };
 
     sql = 'INSERT INTO users SET ?';
@@ -62,10 +69,37 @@ router.post('/register', function(req, res) {
         return res.sendStatus(500);
       }
 
-      response.success = true;
-      res.send(response);
+      createToken(req.db, email, function(token) {
+        response.success = true;
+        response.token = token;
+        res.send(response);
+      });
     });
   });
 });
+
+function createToken(db, email, callback) {
+  var created = new Date();
+  var expiry = new Date(created + 7);
+  var token = crypto
+    .createHash('md5')
+    .update(email + ':' + created.getTime())
+    .digest('hex');
+
+  var data = {
+    created: created.getTime(),
+    expiry: expiry.getTime(),
+    token: token
+  };
+
+  sql = 'INSERT INTO tokens SET ?';
+  db.query(sql, data, function(err, result) {
+    if (err) {
+      return res.sendStatus(500);
+    }
+
+    callback(token);
+  });
+}
 
 module.exports = router;
